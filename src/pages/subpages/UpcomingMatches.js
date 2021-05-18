16c6839ea1555a1f5543e9d0446a90fa5b0c4a9a
Pawcio <paulanersilva@gmail.com>
2021-05-18 19:30:06 +0200

    Add upcoming matches component

--- src/pages/subpages/UpcomingMatches.js
+++ src/pages/subpages/UpcomingMatches.js
@@ -1,8 +1,41 @@
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router";
+import { getFixtures } from "../../api"
+
 export default function UpcomingMatches() {
+    const [matches, setMatches] = useState([]);
+    const location = useLocation();
+    const date = new Date();
+    const dateWeek = new Date();
+    
+    dateWeek.setDate(date.getDate() + 7)
+    
+    let leagueKey = 0;
+    if(location.pathname.includes("bundesliga")) leagueKey=195;
+    if(location.pathname.includes("ligue1")) leagueKey=176;
+    if(location.pathname.includes("seriea")) leagueKey=262;
+    if(location.pathname.includes("laliga")) leagueKey=468;
+    if(location.pathname.includes("premierleague")) leagueKey=148;
+
+    useEffect(() => {
+        const fetchData = async () => {
+            const result = await getFixtures(date, dateWeek, leagueKey).catch((err) => {
+                console.error(err);
+            });
+            setMatches(result.data.result);
+        };
+        fetchData();
+    }, [leagueKey]);
+
     return (
         <>
-            <div>
-                <h1>Upcoming Matches</h1>  
+            <div class="container-lg">
+                <h1>Upcoming matches</h1>
+                {matches && matches.map((match) => {
+                    return (
+                        <p>{match.event_home_team} - {match.event_away_team}</p>
+                    )
+                })}
             </div>
         </>
     );
