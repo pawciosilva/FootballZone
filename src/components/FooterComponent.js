8628c3a4e35a0762475ad8b05dae2f791a00a321
Pawcio <paulanersilva@gmail.com>
2021-06-02 16:13:47 +0200

    Add loading circle in all pages and change footer

--- src/components/FooterComponent.js
+++ src/components/FooterComponent.js
@@ -3,79 +3,43 @@ export default function FooterComponent() {
     <footer class="pt-4 my-md-5 pt-md-5 text-center border-top">
       <div class="row my-1">
         <div class="col-6 col-md">
-          <h5>Bla bla</h5>
+          <h5>Twórcy</h5>
           <ul class="list-unstyled text-small">
             <li class="mb-1">
-              <a class="link-secondary text-decoration-none" href="#">
-                Cool stuff
+              <a class="link-secondary text-decoration-none" href="https://github.com/GizoPL" target="_blank">
+                Artur Giza
               </a>
             </li>
             <li class="mb-1">
-              <a class="link-secondary text-decoration-none" href="#">
-                Random feature
+              <a class="link-secondary text-decoration-none" href="https://github.com/pawciosilva" target="_blank">
+                Paweł Magdziarz
               </a>
             </li>
             <li class="mb-1">
-              <a class="link-secondary text-decoration-none" href="#">
-                Team feature
+              <a class="link-secondary text-decoration-none" href="https://github.com/Blazevarjo" target="_blank">
+                Jan Rubel
               </a>
             </li>
             <li class="mb-1">
-              <a class="link-secondary text-decoration-none" href="#">
-                Stuff for developers
+              <a class="link-secondary text-decoration-none" href="https://github.com/Presoon" target="_blank">
+                Kamil Szpak
+              </a>
+            </li>
+            <li class="mb-1">
+              <a class="link-secondary text-decoration-none" href="https://github.com/JustD4nTe" target="_blank">
+                Dominik Tyc
+              </a>
+            </li>
+            <li class="mb-1">
+              <a class="link-secondary text-decoration-none" href="https://github.com/Jakub-19" target="_blank">
+                Jakub Wadas
               </a>
             </li>
           </ul>
         </div>
-        <div class="col-6 col-md">
-          <h5>Bla bla bla</h5>
-          <ul class="list-unstyled text-small">
-            <li class="mb-1">
-              <a class="link-secondary text-decoration-none" href="#">
-                Resource
-              </a>
-            </li>
-            <li class="mb-1">
-              <a class="link-secondary text-decoration-none" href="#">
-                Resource name
-              </a>
-            </li>
-            <li class="mb-1">
-              <a class="link-secondary text-decoration-none" href="#">
-                Another resource
-              </a>
-            </li>
-            <li class="mb-1">
-              <a class="link-secondary text-decoration-none" href="#">
-                Final resource
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div class="col-6 col-md">
-          <h5>Bla bla bla</h5>
-          <ul class="list-unstyled text-small">
-            <li class="mb-1">
-              <a class="link-secondary text-decoration-none" href="#">
-                Team
-              </a>
-            </li>
-            <li class="mb-1">
-              <a class="link-secondary text-decoration-none" href="#">
-                Locations
-              </a>
-            </li>
-            <li class="mb-1">
-              <a class="link-secondary text-decoration-none" href="#">
-                Privacy
-              </a>
-            </li>
-            <li class="mb-1">
-              <a class="link-secondary text-decoration-none" href="#">
-                Terms
-              </a>
-            </li>
-          </ul>
+        <div class="col-6 col-md my-5">
+          <h3>Made by GSMWTR &#169;</h3>
+          <h5>2021</h5>
         </div>
       </div>
     </footer>
--- src/pages/subpages/Table.js
+++ src/pages/subpages/Table.js
@@ -1,8 +1,10 @@
 import { useEffect, useState } from "react";
 import { useLocation } from "react-router";
 import { getStandings } from "../../api";
+import HashLoader from "react-spinners/HashLoader";
 
 export default function Table() {
+  const [loading, setLoading] = useState(true);
   const [teams, setTeams] = useState([]);
   const location = useLocation();
 
@@ -19,6 +21,7 @@ export default function Table() {
         console.error(err);
       });
       setTeams(result.data.result.total);
+      setLoading(false);
     };
     fetchData();
   }, [leagueKey]);
@@ -28,53 +31,57 @@ export default function Table() {
       <div className="container my-5 text-center">
         <div className="row justify-content-center">
           <div className="col-md-12 col-lg-10">
-            <table class="table mt-4" id="league-table">
-              <thead>
-                <tr>
-                  <th scope="col" className="text-center">
-                    Position
-                  </th>
-                  <th scope="col" className="text-center">
-                    Team
-                  </th>
-                  <th scope="col" className="text-center">
-                    Points
-                  </th>
-                  <th scope="col" className="text-center">
-                    W
-                  </th>
-                  <th scope="col" className="text-center">
-                    D
-                  </th>
-                  <th scope="col" className="text-center">
-                    L
-                  </th>
-                  <th scope="col" className="text-center">
-                    Goal Diffrence
-                  </th>
-                </tr>
-              </thead>
-              {teams &&
-                teams.map((team) => {
-                  return (
-                    <tbody>
-                      <tr>
-                        <td className="text-center fw-bold">
-                          {team.standing_place}.
-                        </td>
-                        <td className="text-center fw-bold">
-                          {team.standing_team}
-                        </td>
-                        <td className="text-center">{team.standing_PTS}</td>
-                        <td className="text-center">{team.standing_W}</td>
-                        <td className="text-center">{team.standing_D}</td>
-                        <td className="text-center">{team.standing_L}</td>
-                        <td className="text-center">{team.standing_GD}</td>
-                      </tr>
-                    </tbody>
-                  );
-                })}
-            </table>
+            {loading ? (
+              <HashLoader color={"#4e80b4"} />
+            ) : (
+              <table class="table mt-4" id="league-table">
+                <thead>
+                  <tr>
+                    <th scope="col" className="text-center">
+                      Position
+                    </th>
+                    <th scope="col" className="text-center">
+                      Team
+                    </th>
+                    <th scope="col" className="text-center">
+                      Points
+                    </th>
+                    <th scope="col" className="text-center">
+                      W
+                    </th>
+                    <th scope="col" className="text-center">
+                      D
+                    </th>
+                    <th scope="col" className="text-center">
+                      L
+                    </th>
+                    <th scope="col" className="text-center">
+                      Goal Diffrence
+                    </th>
+                  </tr>
+                </thead>
+                {teams &&
+                  teams.map((team) => {
+                    return (
+                      <tbody>
+                        <tr>
+                          <td className="text-center fw-bold">
+                            {team.standing_place}.
+                          </td>
+                          <td className="text-center fw-bold">
+                            {team.standing_team}
+                          </td>
+                          <td className="text-center">{team.standing_PTS}</td>
+                          <td className="text-center">{team.standing_W}</td>
+                          <td className="text-center">{team.standing_D}</td>
+                          <td className="text-center">{team.standing_L}</td>
+                          <td className="text-center">{team.standing_GD}</td>
+                        </tr>
+                      </tbody>
+                    );
+                  })}
+              </table>
+            )}
           </div>
         </div>
       </div>
